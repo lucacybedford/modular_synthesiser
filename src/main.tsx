a36ddf2e533e0e2550156ac0201351de1cf4f3b1
lucabedford <lucacybedford@gmail.com>
2025-01-23 02:06:20 +0000

    added vercel analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <><App/><Analytics/></>
 )
